9680dde6363e357c7fe1de3b0ef3ebd7dcf7a61f
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-08-23 17:42:29 +0200

    Update versions.sh

--- versions.sh
+++ versions.sh
@@ -1,22 +1,5 @@
 master
 ###########################-VERSIONS-PRIOR
-8.7.4
-8.7.3
-8.7.2
-8.7.1
-8.7.0
-8.6.10
-8.6.9
-8.6.8
-8.6.7
-8.6.6
-8.6.5
-8.5.8
-###### DONT RECOMMEND BELOW (ARCHIEVED VERSIONS)
-8.5.7
-8.5.4
-8.4.20
-8.4.18
-8.3.17
+master
 ###########################-COMMANDS-BELOW-FOR-TESTING
 dev
